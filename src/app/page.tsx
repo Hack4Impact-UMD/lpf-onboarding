ae2cde489ebe2e614e2494d6b04f0f7e2f35ff8c
home0712 <k07m12j@gmail.com>
2024-02-12 12:59:30 -0500

    [ADD] countButton, button CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import React, { useState } from "react";
+import style from "@/app/page.module.css";
+
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count => count + 1);
+  }
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count => count - 1);
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -26,6 +43,15 @@ export default function Home() {
             />
           </a>
         </div>
+      </div>
+
+      <div className={style.container}>
+        <button className={style.leftButton} onClick={decrease}>-</button>
+        <div className={style.innerContainer}>
+            <p className={style.title}>Counter</p>
+            <p className={style.text}>{count}</p>
+        </div>
+        <button className={style.rightButton} onClick={increase}>+</button>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
