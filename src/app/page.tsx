22c745c7f1ee613cf9e6997db0e233ef60b05266
Jessica Nguyen <nguyen15@terpmail.umd.edu>
2024-02-18 22:15:20 -0500

    added display for counter and buttons to toggle count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+"use client";
 import Image from "next/image";
+import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  return <p>Hello World!</p>
+  const [counter, setCounter] = useState(1);
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <button className={styles.decrement} onClick={() => setCounter(counter - 1)} />
+        <div className={styles.countDisplay}>
+          <h1 className={styles.displayHeader}>Counter</h1>
+          <p className={styles.counter}>{counter}</p>
+        </div>
+        <button className={styles.increment} onClick={() => setCounter(counter + 1)} />
+      </div>
+    </div>
+  );
 }
