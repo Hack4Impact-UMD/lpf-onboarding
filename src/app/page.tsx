c4b39823ef8cccd782e50b1ae7400e478283e925
rohitkommuru <rohitkommuru@gmail.com>
2024-09-20 20:10:47 -0400

    rohit kommuru project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import React, { useState } from 'react';
+import "./page.css"
+
+let count = 0
 
 export default function Home() {
-  return <p>Hello World!</p>
-}
+  const [localCount, setLocalCount] = useState(count)
+
+  const deprecate = () =>  { 
+    count -= 1;
+    setLocalCount(count)
+  };
+
+  const increment = () => { 
+    count += 1; 
+    setLocalCount(count)
+  }
+
+  return (
+  <div className="background">
+    <button className='redButton' onClick={deprecate}></button>
+  <div className="Center">
+    <h1 className="center-text"> Counter </h1>
+    <h2> {localCount} </h2> 
+  </div>
+  
+  <button className='greenButton' onClick={increment}></button>
+  </div>
+  )
+};
