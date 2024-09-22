e115f9552ff609a3c124cbddaf297dff2e2c09b2
Varun Rao <varunvellorerao@gmail.com>
2024-09-22 18:22:09 -0400

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
-import Image from "next/image";
+"use client";
+import { useState } from 'react';
+import './page.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <p>Hello World!</p>
+
+  const [counter, setCounter] = useState(0);
+
+  const decrementCounter = () => {
+    let temp = counter;
+    setCounter(temp - 1);
+  }
+
+  const incrementCounter = () => {
+    let temp = counter;
+    setCounter(temp + 1);
+  }
+
+  return (
+    <div className='body-section'>
+      <div className='app-section'>
+        <div className='decrement-section'>
+          <button onClick={decrementCounter}></button>
+        </div>
+        <div className='counter-section'>
+          <h1 className={`${inter.className} counter-title`}>Counter</h1>
+          <h2 className={`${inter.className} counter`}>{ counter }</h2>
+        </div>
+        <div className='increment-section'>
+          <button onClick={incrementCounter}></button>
+        </div>
+      </div>
+    </div>
+  )
 }
